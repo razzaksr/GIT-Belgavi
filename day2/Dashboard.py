74dcb3a5529758ead61d1d6cf05e585e8619b10e
razzaksr <razzaksr@gmail.com>
2026-04-22 15:02:41 +0530

    build2

--- day2/Dashboard.py
+++ day2/Dashboard.py
@@ -3,4 +3,5 @@ from API import gHealth
 health1 = gHealth.getHealth()
 print(health1.filter("bmr",1400,"ge"))
 health1.initialize("Sabarinathan",34,"male",172)
-health1.makeWeighIn(id="4",weight=82.9)
+# health1.makeWeighIn(id="4",weight=82.9)
+health1.updateWeighIn(id="4",weight=78)
